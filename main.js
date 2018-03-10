96be972f1b0df406ecfb4f8b07bd96a9bd8e4f3d
Eva Datcenko <evarektor@gmail.com>
2018-03-11 00:50:29 +0200

    init

---
+++ main.js
@@ -0,0 +1,57 @@
+var string = 'string'; 
+		var num = 3;
+		var bull = false;
+		var arr = ['ad', 1, 21];
+		var object = {123:12};
+		alert(string, num, bull, arr, object);
+		console.log(string, num, bull, arr, object);
+
+var string ='Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
+	console.log(string);
+	console.log(string.toUpperCase());
+
+	console.log(string.toLowerCase());
+
+var a, b, c, d;
+	a = 10;
+	a = b = c = d = 5;
+	alert(a);
+	console.log(a);
+
+var a, b, c;
+	a = b = c = 2 + 2;
+		alert(a);
+		alert(b);
+		alert(c);
+		console.log(a, b, c);
+
+var n = 248;
+	console.log(n.toFixed(2));
+
+var i = 4;
+	console.log(++i);
+	console.log(i++);
+
+var i = 4;
+	console.log(i++);
+	console.log(i);
+ 
+var n = 2;
+	n+= 5;
+	n*= 2;
+	n/= 2;
+	n-= 1; 
+	alert(n);
+	console.log(n);
+
+console.log(Math.PI);
+console.log(Math.round(89.279));
+console.log(Math.random()*10);
+console.log(Math.pow(2,5));
+
+console.log('Lorem ipsum dolor sit amet, consectetur adipisicing elit.'.length);
+var string = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
+	console.log(string.indexOf('sit'));
+	console.log(string.replace('sit', 'sad'));
+	string = string.replace('sit', 'sad');
+	
